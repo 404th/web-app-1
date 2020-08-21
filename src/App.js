6663af3be11ffd9a2a67cb282a8a3dc9a5989361
myevenway <umarov.doniyor.2002@gmail.com>
2020-08-21 17:49:44 +0500

    Committed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 //COMPONENTS
 import Home from './pages/Home'
 import About from './pages/About'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <Router>
       <Route exact path='/' component={ Home } />
       <Route exact path='/about' component={ About } />
+      <Route exact path='/contact' component={ Contact } />
     </Router>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,6 +26,9 @@ const Navbar = () => {
             <li className="nav-item">
               <NavLink className="nav-link" to={'/about'}>About</NavLink>
             </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={'/contact'}>Contact</NavLink>
+            </li>
           </ul>
         </div>
       </div>
